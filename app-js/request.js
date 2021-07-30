13742e0b7c03891d2da8b302aed3baad6a4db8d2
Dev Front <developermater26@gmail.com>
2021-07-30 15:25:56 -0600

    se hizo el manejo de errores

--- app-js/request.js
+++ app-js/request.js
@@ -2,8 +2,16 @@ import {Countries} from "./helpers/countries.js";
 export const GetAll = async(url)=>{
     try {
         let res = await fetch(url || 'https://restcountries.eu/rest/v2/all') 
-        
+        if(!res.ok)throw {
+            status:res.status,
+            message_status:res.statusText
+        }    
     } catch (err) {
-        
+        console.log(err.status);
+        if(err.status === 404){
+            alert(`I'm sorry, an error occurred when recovering the countries `)        
+        }else{
+            alert(`I'm sorry, something unexpected happened Error ${err.status} ${err.message_status}`)        
+        }
     }
 }
