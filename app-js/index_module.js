584dd7bcce7bdba9987c08a1a55de7ffedeb68ae
Dev Front <developermater26@gmail.com>
2021-07-30 13:22:59 -0600

    se importo la funcion form y se hizo hizo un appendChild al root

--- app-js/index_module.js
+++ app-js/index_module.js
@@ -1,5 +1,7 @@
 import {header} from "./helpers/header.js";
+import { Form } from "./helpers/form.js";
 const $root = document.querySelector('#root')
 document.addEventListener('DOMContentLoaded',(e)=>{
     $root.appendChild(header())
+    $root.appendChild(Form())
 })
