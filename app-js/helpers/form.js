170f42cca4f34baa8742dfa54298c69fe7242483
Dev Front <developermater26@gmail.com>
2021-07-29 23:02:58 -0600

    se le dieron estilos a las vas variables y al input los atibutos name,type,placeholder,id

--- app-js/helpers/form.js
+++ app-js/helpers/form.js
@@ -4,4 +4,16 @@ export const Form =()=>{
     const $searchIcon = document.createElement('i')
     const $searchInput = document.createElement('input')
     const $select = document.createElement('select')
+
+    $form.id = 'form'
+    $SearchBox.classList.add('search-box')
+    $searchIcon.classList.add('fas','fa-search','search__icon')
+    $searchInput.classList.add('search')
+    $searchInput.name = 'search'
+    $searchInput.type = 'search'
+    $searchInput.id = 'search-input'
+    $searchInput.placeholder = 'Search for a country...'
+    $select.name = 'select'
+    $select.id = 'select-region'
+
 }
