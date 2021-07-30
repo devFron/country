df6c2b71d0cb57eea5bc035c2ad909f3664470da
Dev Front <developermater26@gmail.com>
2021-07-29 22:57:55 -0600

    se creo el formulario y el input con el select

--- app-js/helpers/form.js
+++ app-js/helpers/form.js
@@ -1,3 +1,7 @@
 export const Form =()=>{
-    
+    const $form = document.createElement('form')
+    const $SearchBox = document.createElement('section') 
+    const $searchIcon = document.createElement('i')
+    const $searchInput = document.createElement('input')
+    const $select = document.createElement('select')
 }
