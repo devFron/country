a03f7867aac2ec71011523274873dbc07ed0d96f
Dev Front <developermater26@gmail.com>
2021-07-29 22:39:27 -0600

    se creo la funcion y las variables del dom para crear el header dinamica mente

---
+++ app-js/helpers/header.js
@@ -0,0 +1,8 @@
+export const header = ()=>{
+    const $header = document.createElement('header')
+    const $title = document.createElement('h2')
+    const $darkModeBox = document.createElement('section')
+    const $darkModeIcon = document.createElement('i')
+    const $darkModeTitle = document.createElement('p')
+    const $Fragment =document.createDocumentFragment()
+}
