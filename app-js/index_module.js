4f7132070d890bc50b095da26053cead9e9df7bc
Dev Front <developermater26@gmail.com>
2021-07-30 15:06:03 -0600

    seimporto la funcion Getall y se ejecuto ala carga del DOM tambien se creo una section que contendra todos los paises y se añadio al root en la carga del DOM

--- app-js/index_module.js
+++ app-js/index_module.js
@@ -1,7 +1,12 @@
 import {header} from "./helpers/header.js";
 import { Form } from "./helpers/form.js";
+import { GetAll } from "./request.js";
 const $root = document.querySelector('#root')
+const $countries = document.createElement('section')
+$countries.classList.add('countries')
 document.addEventListener('DOMContentLoaded',(e)=>{
     $root.appendChild(header())
     $root.appendChild(Form())
+    $root.appendChild($countries)
+    GetAll()
 })
