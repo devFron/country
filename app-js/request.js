b7e313090311e831a8c25c538e61b9305c08b5e2
Dev Front <developermater26@gmail.com>
2021-07-30 15:10:53 -0600

    se hizo una peticon fetch con la url que trae a todos los paises si no le pasas nada por el parametro url

--- app-js/request.js
+++ app-js/request.js
@@ -1,6 +1,7 @@
 import {Countries} from "./helpers/countries.js";
 export const GetAll = async(url)=>{
     try {
+        let res = await fetch(url || 'https://restcountries.eu/rest/v2/all') 
         
     } catch (err) {
         
