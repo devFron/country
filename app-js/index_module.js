6e0d8d085f7abfef5c991c67680eedaea88edf09
Dev Front <developermater26@gmail.com>
2021-07-31 11:44:17 -0600

    se creo un boton para poder subir hasta la cabecera de la pagina los listeners de submit, click, change, scroll se scaron de el DOMContentLoaded y el formulario se capturo directamente con la etiqueta

--- app-js/index_module.js
+++ app-js/index_module.js
@@ -4,33 +4,63 @@ import { GetAll } from "./request.js";
 const $root = document.querySelector('#root')
 const $countries = document.createElement('section')
 $countries.classList.add('countries')
+const $upButton = document.createElement('section')
+$upButton.classList.add('up')
+$upButton.innerHTML = `<i class="fas fa-arrow-up up__icon"></i>`
+$upButton.style.display = 'none'
 document.addEventListener('DOMContentLoaded',(e)=>{
     $root.appendChild(header())
     $root.appendChild(Form())
     $root.appendChild($countries)
-    GetAll()
-    const $form = $root.querySelector('#form')
-    document.addEventListener('submit',(e)=>{
-        if(e.target === $form)e.preventDefault()
+    $root.appendChild($upButton)
+    GetAll({
+        filter:false
     })
-    document.addEventListener('click',(e) =>{
-        if(e.target.matches('.search__icon')){
-            let nameOfCountry = $form.search.value.toLowerCase()
-            let expreg = /^[0-9]+$/;
-            if(expreg.test(nameOfCountry)){
-                alert('please enter a valid country')
-            }
-            GetAll(`https://restcountries.eu/rest/v2/name/${nameOfCountry}`)
+})
+
+document.addEventListener('submit',(e)=>{if(e.target.matches('form')){e.preventDefault()}})
+
+document.addEventListener('click',(e) =>{
+
+    if(e.target.matches('.search__icon')){
+
+        let nameOfCountry = document.querySelector('#form').search.value.toLowerCase()
+        let expreg = /^[0-9]+$/;
+        if(expreg.test(nameOfCountry)){
+            alert('please enter a valid country')
         }
-    })
-    let select = document.querySelector('#select-region')
-    document.addEventListener('change',(e)=>{
-        if(e.target === select){
-            if(e.target.value === 'Filter by Region'|| e.target.value === 'America'){
-                return
-            }else{
-                GetAll(`https://restcountries.eu/rest/v2/region/${e.target.value.toLowerCase()}`)
-            }
+
+        GetAll({
+            url:`https://restcountries.eu/rest/v2/name/${nameOfCountry}`,
+            filter:true
+        })
+    }
+
+    if(e.target.matches('.up')|| e.target.matches('.up__icon')){
+        window.scrollTo({
+            top:0,
+            behavior:'smooth'
+        })
+    }
+})
+
+document.addEventListener('change',(e)=>{
+    if(e.target.matches('select')){
+        if(e.target.value === 'Filter by Region'|| e.target.value === 'America'){
+            return
+        }else{
+            GetAll({
+                url:`https://restcountries.eu/rest/v2/region/${e.target.value.toLowerCase()}`,
+                filter:true
+            })
         }
-    })
+    }
+})
+
+window.addEventListener('scroll',(e)=>{
+    if(window.pageYOffset > 10){
+        $upButton.style.display = 'flex'
+    }else{
+        $upButton.style.display = 'none'
+    }
 })
