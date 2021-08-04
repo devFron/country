834912e9b64913642b689e3a4c66a27a3b3c6ba3
Dev Front <developermater26@gmail.com>
2021-08-04 17:09:29 -0600

    el boton de dak-mode se cambio de un section a un button

--- app-js/helpers/header.js
+++ app-js/helpers/header.js
@@ -1,7 +1,7 @@
 export const header = ()=>{
     const $header = document.createElement('header')
     const $title = document.createElement('h2')
-    const $darkModeBox = document.createElement('section')
+    const $darkModeBox = document.createElement('button')
     const $darkModeIcon = document.createElement('i')
     const $darkModeTitle = document.createElement('p')
     const $Fragment =document.createDocumentFragment()
